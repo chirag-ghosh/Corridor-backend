45b0df1bcc55c562a777a5f23a586427c7ef018e
chirag828049 <cghosh828049@gmail.com>
2021-07-25 16:48:03 +0530

    Connected to MongoDB Atlas and set api for signup, signin and posts

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURL: "mongodb+srv://apiHandler:autocat@corridor.dkzy2.mongodb.net/Corridor?retryWrites=true&w=majority",
+    secretOrKey: "Never gonna give you up"
+}
---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+const user = require("./routes/api/user");
+const post = require("./routes/api/post");
+
+const App = express();
+App.use(express.urlencoded({ extended: false }));
+App.use(express.json());
+
+App.use(passport.initialize());
+require('./config/passport')(passport);
+
+
+const db = require('./config/keys').mongoURL;
+
+mongoose
+    .connect(db, {useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("MongoDB connected"))
+    .catch(err => console.log(err));
+
+//test route for debugging
+App.get("/", (req, res) => {
+    res.send("Welcome to Corridor api");
+})
+
+App.use("/", user);
+App.use("/post", post);
+
+const PORT = process.env.PORT || 5000;
+App.listen(PORT);
+console.log(`APP Started on ${PORT}`);
