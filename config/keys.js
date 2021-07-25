d33a7966bc008e6f53e79fd9cb8e8d43c1e1e0ff
chirag828049 <cghosh828049@gmail.com>
2021-07-25 18:52:38 +0530

    Secured api keys

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURL: "mongodb+srv://apiHandler:autocat@corridor.dkzy2.mongodb.net/Corridor?retryWrites=true&w=majority",
-    secretOrKey: "Never gonna give you up"
-}
+  mongoURL: process.env.mongoURL,
+  secretOrKey: process.env.secretOrKey || "Never gonna give you up",
+};
